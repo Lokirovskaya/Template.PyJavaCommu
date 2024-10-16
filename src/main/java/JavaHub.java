5271c6d2958c9df15e3c49305a16d6731d946712
Lokii_ThinkBook <719220502@qq.com>
2024-10-16 14:50:32 +0800

    exception control

--- src/main/java/JavaHub.java
+++ src/main/java/JavaHub.java
@@ -6,19 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
+@SuppressWarnings("ALL")
 public class JavaHub {
     private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    ObjectMapper mapper = new ObjectMapper();
-
-    public <T> T receive(Class<T> cls) throws IOException {
-        String rawJSON = reader.readLine();
-        return mapper.readValue(rawJSON, cls);
-    }
-
-    public <T> List<T> receiveList(Class<T> cls) throws IOException {
-        String rawJSON = reader.readLine();
-        return mapper.readValue(rawJSON, mapper.getTypeFactory().constructCollectionType(List.class, cls));
-    }
+    private final ObjectMapper mapper = new ObjectMapper();
 
     private static class Response {
         public final boolean error = false;
@@ -30,22 +21,42 @@ public class JavaHub {
         public String message;
     }
 
-    public void sendResponse(Object obj) throws IOException {
-        Response response = new Response();
-        response.data = obj;
-        String responseJSON = mapper.writeValueAsString(response);
-        System.out.println(responseJSON);
+    private String readPipe() throws IOException {
+        String line;
+        while ((line = reader.readLine()) == null) ;
+        return line;
     }
 
-    public void sendError(String message)  {
+    public <T> T receive(Class<T> cls) throws IOException {
+        String rawJSON = readPipe();
+        return mapper.readValue(rawJSON, cls);
+    }
+
+    public <T> List<T> receiveList(Class<T> cls) throws IOException {
+        String rawJSON = readPipe();
+        return mapper.readValue(rawJSON, mapper.getTypeFactory().constructCollectionType(List.class, cls));
+    }
+
+    public void sendResponse(Object obj) {
+        Response response = new Response();
+        response.data = obj;
+        try {
+            String responseJSON = mapper.writeValueAsString(response);
+            System.out.println(responseJSON);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void sendError(String message) {
         Error error = new Error();
         error.message = message;
         String errorJSON;
         try {
             errorJSON = mapper.writeValueAsString(error);
+            System.out.println(errorJSON);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        System.out.println(errorJSON);
     }
 }
